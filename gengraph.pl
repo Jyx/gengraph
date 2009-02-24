a8aac730fc1f5c02d3eb8f94c1ac71ae192b81b0
Joakim Bech <joakim.bech@gmail.com>
2009-02-24 21:35:10 +0100

    Adding files to a hash array exist now, but it isn't 100% working as it is supposed to do.

--- gengraph.pl
+++ gengraph.pl
@@ -4,6 +4,7 @@
 ################################################################################
 use strict;
 use warnings;
+use Cwd;
 use File::Find;
 
 ################################################################################
@@ -18,11 +19,12 @@ my $picture_ext = "jpg";
 my $root_folder = ".";
 
 my %existing_files;
+my %existing_folders;
 
 ################################################################################
 #  Sub routines                                                                #
 ################################################################################
-sub check_input {
+sub parse_inparameters {
 	foreach my $arg (@ARGV) {
 		if ($arg =~ m/^-d$/) {
 			$debug = 1;
@@ -62,26 +64,102 @@ sub show_globals {
 }
 
 sub make_graph {
-	print "Generating $graphvis graph\n";
-	system("$graphvis -T$picture_ext $dotfile\.$dotextension -o $dotfile\_$graphvis\.$picture_ext");
+	my $filename = "$dotfile\.$dotextension";
+	if (-e $filename) {
+		print "Generating $graphvis graph\n";
+		system("$graphvis -T$picture_ext $filename -o $dotfile\_$graphvis\.$picture_ext");
+	} else {
+		print "Couldn't open file $filename in folder " . getcwd . "\n";
+	}
 }
 
 sub save_files {
-	print "File: $_\n" if $debug;
-	print "Dir:  $File::Find::dir\n\n" if $debug;
-	$existing_files{$_} = $File::Find::dir;
+	if ($_ =~ m/\.[ch]$/) {
+		my $file = $_;
+		my $dir = $File::Find::dir;
+
+		print "File: $file\n" if $debug;
+		print "Dir:  $dir\n\n" if $debug;
+
+		# Store the file name
+		$existing_files{$file} = $dir;
+
+		my @splitted_folder = split(/\//, $dir);
+		# This is tricky, we have some special cases here. First we need to take
+		# care of when the number of subfolder is greater than the provided
+		# level variable. All those files in sub directories further down we
+		# treat as a if they exist in the same sub folder.
+		#
+		# The next case is when files exist between 1 and the variable level
+		# number of sub folders.
+		#
+		# The last case, the else, is when files are located in the root folder.
+		if (scalar(@splitted_folder) > $level) {
+			my $dir_string;
+			for my $i (0 .. $level) {
+				$dir_string = $dir_string . "$splitted_folder[$i]";
+				$dir_string = $dir_string . "\/" if ($i < $level);
+				#print "dir_string: $dir_string\n" if $debug;
+			}
+			print "Greater than level\n";
+			push(@{$existing_folders{$dir}}, $file);
+		} elsif (scalar(@splitted_folder) > 1 &&
+				 scalar(@splitted_folder) <= $level) {
+
+			print "2 to level\n";
+		} else {
+			print "Zero\n";
+		}
+	}
 }
 
-sub generate_file_list {
-	chdir($_[0]);
-	find(\&save_files, ".");
-	&print_file_list if $debug;
+sub generate_file_lists {
+	my $root = getcwd;
+	if (-d $_[0]) {
+		chdir($_[0]);
+		find(\&save_files, ".");
+		&print_file_list if $debug;
+		&print_folder_list if $debug;
+		chdir($root);
+	} else {
+		print "Unknown root folder: $_[0]\n";
+	}
 }
 
 sub print_file_list {
-	print "File list\n=========\n";
-	foreach my $key (sort keys %existing_files) {
-		print "$key $existing_files{$key}\n";
+	print "\nFile list\n=========\n";
+	foreach my $key (sort {$existing_files{$a} cmp $existing_files{$b}}
+					 keys %existing_files) {
+		print "  $key $existing_files{$key}\n";
+	}
+}
+
+sub print_folder_list {
+	print "\nFolder list (" . keys (%existing_folders) . ")\n";
+	print "===================\n";
+	foreach my $key (keys %existing_folders) {
+		print "$key $existing_folders{$key}\n";
+		foreach my $file (@{$existing_folders{$key}}) {
+			print "  $file\n";
+		}
+	}
+}
+
+sub is_file_known {
+	my $file_to_check = $_[0];
+	if ($existing_files{$file_to_check}) {
+		print "File exist on path $existing_files{$file_to_check}\n" if $debug;
+		return 1;
+	}
+	return 0;
+}
+
+sub make_subgraphs {
+	foreach my $key (sort {$existing_files{$a} cmp $existing_files{$b}}
+					 keys %existing_files) {
+		my @splitted_folder = split(/\//, $existing_files{$key});
+		#map { print "$_\n" } @splitted_folder if $debug;
+		#print "Size: " .scalar(@splitted_folder) . "\n";
 	}
 }
 
@@ -89,8 +167,9 @@ sub print_file_list {
 #  Main program                                                                #
 ################################################################################
 print "gengraph - A script for making dependency graph for source code\n\n";
-&check_input;
+&parse_inparameters;
 &show_globals if $debug;
-&generate_file_list($root_folder);
+&generate_file_lists($root_folder);
+#&make_subgraphs;
 
 &make_graph if $graphvis;
